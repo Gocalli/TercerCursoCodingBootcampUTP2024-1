35477a45509a057972fe79efb6ddf572bf5c153c
unknown <santiago.castano@utp.edu.co>
2024-06-10 17:05:00 -0500

    añadiendo estilo a la calculadora

--- React/calculadora/src/componentes/controlladorButton.jsx
+++ React/calculadora/src/componentes/controlladorButton.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export function Buttons(props){
     return(
-        <button className="numbers" onClick={() => props.saveNumb(props.children)}>{props.children}</button>
+        <button className="numbers-button" onClick={() => props.saveNumb(props.children)}>{props.children}</button>
     )
 }
